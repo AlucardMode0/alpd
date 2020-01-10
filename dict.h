9da9c80b7163f9cab792391bad0b699c7ae12120
AlucardMode0 <44033200+AlucardMode0@users.noreply.github.com>
2020-01-10 15:00:26 +0200

    Update dict.h

--- dict.h
+++ dict.h
@@ -1,11 +1,10 @@
 #ifndef _DICT_
 #define _DICT_
-
+#include <ctype.h>
 extern int total;
 typedef struct {
     char *key;
-    int *value;
-	 int counter;
+    int value;
 } KVPair;
 
 typedef struct Dictionary_t {
@@ -14,11 +13,12 @@ typedef struct Dictionary_t {
 } Dictionary;
 
 Dictionary* dict_new();
-void dict_add(Dictionary *dictionary, const char *key, int value);
+void dict_add(Dictionary *dictionary,  char *key);
 int dict_has(Dictionary *dictionary, const char *key);
 int dict_get(Dictionary *dictionary, const char *key);
 void dict_remove(Dictionary *dictionary, const char *key);
 void dict_free(Dictionary *dictionary);
 void dict_print_key(Dictionary *dictionary, const char *key);
 void dict_print(Dictionary *dictionary) ;
+void dict_to_dirs(Dictionary *dictionary,char *file) ;
 #endif
